8c3a1d2d1d808c011ea53f843a26b80ced751054
Sandesh Bharambe <109981156+Sandesh0527@users.noreply.github.com>
2024-08-24 16:02:01 +0530

    Delete Assignment 1 directory

--- Assignment 1/Rectangle.java
+++
@@ -1,9 +0,0 @@
-class Rectangle {
-public static void main (String args[]) {
-int L = 15;
-int B = 12;
-int Area =  L*B ;
-System.out.println("The area of Rectangle is :" + Area);
-}
-
-}
--- Assignment 1/Square.java
+++
@@ -1,7 +0,0 @@
-class Square {
-public static void main (String args[]) {
-int side = 5;
-int Area = side*side ;
-System.out.println("The area of Square is :" + Area);
-}
-}
