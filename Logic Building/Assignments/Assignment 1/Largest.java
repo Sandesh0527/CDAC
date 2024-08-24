77e0102f40173f46b80493fbb531b278aa91ef48
Sandesh Bharambe <109981156+Sandesh0527@users.noreply.github.com>
2024-08-24 16:01:34 +0530

    Add files via upload

---
+++ Logic Building/Assignments/Assignment 1/Largest.java
@@ -0,0 +1,17 @@
+class Largest {
+public static void main(String args[]) {
+int N1 = 10;
+int N2= 20;
+int N3 = 30;
+if(N1>N2 && N1>N3) {
+System.out.println("The Largest Number is N1" );
+
+}
+else if(N2>N1 && N2>N3) {
+System.out.println("The Largest Number is N2");
+}
+else {
+System.out.println("Largest Number is N3")
+;}
+}
+}
