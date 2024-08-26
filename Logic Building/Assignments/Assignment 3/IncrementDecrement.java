2d3ef85e8d34ffb020643b97df4b5100e0a7ce53
Sandesh Bharambe <109981156+Sandesh0527@users.noreply.github.com>
2024-08-26 16:25:02 +0530

    Add files via upload

---
+++ Logic Building/Assignments/Assignment 3/IncrementDecrement.java
@@ -0,0 +1,11 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int result = ++a + b--;
+
+        System.out.println("Result: " + result);
+        
+    }
+}
